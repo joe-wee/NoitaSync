31b3b26101c3ac947bd87920deb45845473a9599
joe-wee <66483285+joe-wee@users.noreply.github.com>
2022-05-09 18:51:49 -0700

    Update NoitaSync.bat
    added light error handling - checks to see if backup dir already exists, if it doesn't, it makes it, if it can't make it, it pauses

--- NoitaSync.bat
+++ NoitaSync.bat
@@ -1,7 +1,19 @@
 @echo off
+:paths
 set source=%LOCALAPPDATA%Low\Nolla_Games_Noita\save00\
 set dest=%HOMEPATH%\Desktop\NoitaSync\
 
-md %dest%
+:directorycheck
+if not exist %dest% (
+    md %dest% 2>nul
+    IF not errorlevel 1 (
+    GOTO backup
+ )
+ELSE goto exit
+)
 
-robocopy %source% %dest% * /e /zb /copyall /dcopy:DAT /mt:32 /r:1 /w:1 /mot:5 /NJH /log+:%dest%\NoitaSyncLog.txt
+:backup
+robocopy %source% %dest% * /e /zb /copyall /dcopy:DAT /mt:32 /r:1 /w:1 /mot:2 /NJH /log+:%dest%\NoitaSyncLog.txt
+
+:exit
+pause
