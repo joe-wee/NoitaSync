61e1af14bce3fc7e4044769078ceaed5e1d3d13e
joe-wee <66483285+joe-wee@users.noreply.github.com>
2022-05-09 18:30:28 -0700

    Update NoitaSync.bat
    added md %dest% to initialize the directory so the script doesn't error out if it doesn't know where to put the log file.

--- NoitaSync.bat
+++ NoitaSync.bat
@@ -1,6 +1,7 @@
 @echo off
 set source=%LOCALAPPDATA%Low\Nolla_Games_Noita\save00\
-set dest=%HOMEPATH%\Desktop\NoitaSaveBackup\
+set dest=%HOMEPATH%\Desktop\NoitaSync\
 
-robocopy %source% %dest% * /e /zb /copyall /dcopy:DAT /mt:32 /r:1 /w:1 /mot:5 /NP /NJH /log+:%dest%\NoitaSync.txt
+md %dest%
 
+robocopy %source% %dest% * /e /zb /copyall /dcopy:DAT /mt:32 /r:1 /w:1 /mot:5 /NJH /log+:%dest%\NoitaSyncLog.txt
